a8012231e43ad4f93f5943c7233b35c988f2310e
Thomas Murimboh <thomas.murimboh@gmail.com>
2026-07-21 20:57:46 -0300

    Deleted html

---
+++ docs/html/search/all_6.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['motor_20control_0',['Motor Control',['../group___motor_control.html',1,'']]],
+  ['motoraccelerationcontrol_1',['motorAccelerationControl',['../class_motor_manager.html#abbaa7351fd9ef1aae2d21b02b2071fbc',1,'MotorManager']]],
+  ['motorcommand_2',['MotorCommand',['../struct_motor_command.html',1,'']]],
+  ['motormanager_3',['MotorManager',['../class_motor_manager.html',1,'']]],
+  ['motormanager_4',['motorManager',['../class_load_cell_manager.html#a9563d38c0f6a0a2b8f96a4a41fb7a82c',1,'LoadCellManager']]],
+  ['motorstates_5',['MotorStates',['../struct_motor_states.html',1,'']]]
+];
---
+++ docs/html/search/variables_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['loadcellstates_0',['loadCellStates',['../class_load_cell_manager.html#ad9b76184c91151c6a29181f76b3272d9',1,'LoadCellManager']]]
+];
